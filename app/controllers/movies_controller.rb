6cc791b390650d44b93bb70c9f8802a44b21bf4f
Federico Hernandez Nater <166563763+fhernandeznater@users.noreply.github.com>
2025-05-09 17:32:46 +0000

    Added with errors view, made some copy changes to match the sample code

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -27,16 +27,16 @@ class MoviesController < ApplicationController
   end
 
   def create
-    the_movie = Movie.new
-    the_movie.title = params.fetch("query_title")
-    the_movie.description = params.fetch("query_description")
-    the_movie.released = params.fetch("query_released")
+    @the_movie = Movie.new
+    @the_movie.title = params.fetch("query_title")
+    @the_movie.description = params.fetch("query_description")
+    @the_movie.released = params.fetch("query_released", false)
 
-    if the_movie.valid?
-      the_movie.save
-      redirect_to("/movies", { :notice => "Movie created successfully." })
+    if @the_movie.valid?
+      @the_movie.save
+      redirect_to("/movies", { :notice => "Movie was successfully created." })
     else
-      redirect_to("/movies", { :alert => the_movie.errors.full_messages.to_sentence })
+      render template: "movies/with_errors"
     end
   end
 
@@ -50,9 +50,9 @@ class MoviesController < ApplicationController
 
     if the_movie.valid?
       the_movie.save
-      redirect_to("/movies/#{the_movie.id}", { :notice => "Movie updated successfully."} )
+      redirect_to("/movies", { :notice => "Movie updated successfully."} )
     else
-      redirect_to("/movies/#{the_movie.id}", { :alert => the_movie.errors.full_messages.to_sentence })
+      redirect_to("/movies/new", { :alert => the_movie.errors.full_messages.to_sentence })
     end
   end
 
