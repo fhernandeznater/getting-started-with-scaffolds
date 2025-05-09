23a18a7bd8fdf9aaa6e69b9b844c470fc3879a80
Federico Hernandez Nater <166563763+fhernandeznater@users.noreply.github.com>
2025-05-09 18:09:28 +0000

    Added missing route and fixed copy

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,14 @@ Rails.application.routes.draw do
   resources :books
   # Routes for the Movie resource:
 
+  get("/movies/new", { :controller => "movies", :action => "new"})
+
   # CREATE
   post("/movies", { :controller => "movies", :action => "create" })
           
   # READ
+
+
   get("/movies", { :controller => "movies", :action => "index" })
   
   get("/movies/:id", { :controller => "movies", :action => "show" })
